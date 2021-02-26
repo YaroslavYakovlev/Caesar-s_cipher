ff3fb43d42036feb5bf2967a96460c93268b6c51
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-02-26 21:53:17 +0500

    циклический алфовит

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,46 @@
 #include <clocale>
 
 std::string encryptCaesar(std::string str, int value){
+    char final = 'z'- value + 1;
     for(int i = 0; i < str.length(); i++){
-        str[i] += value;
+        if(str[i] < final){
+            str[i] += value;
+        }else{
+            str[i] = 'a' + str[i] - final;
+        }  
     }
     return str;
 }
 
 std::string decryptCaesar(std::string str, int value){
+    char start = 'a' + value - 1;
     for(int i = 0; i < str.length(); i++){
-        str[i] -= value;
+        if(str[i] > start){
+            str[i] -= value;
+        }else { 
+            str[i] = 'z' - str[i] + start;
+        }
     }
     return str;
 } 
 
 int main(){
+    setlocale(LC_ALL,"Russian");
     std::string strEncrypt, strDecrypt;
     int valueEncrypt, valueDecrypt;
+
+    std::cout << "Enter the string to encode" << std::endl;
     std::cin >> strEncrypt;
+    std::cout << "Enter the offset value" << std::endl;
     std::cin >> valueEncrypt;
+
     std::cout << encryptCaesar(strEncrypt, valueEncrypt) << std::endl;
 
+    std::cout << "Enter the string to decrypt" << std::endl;
     std::cin >> strEncrypt;
+    std::cout << "Enter the offset value" << std::endl;
     std::cin >> valueEncrypt;
     std::cout << decryptCaesar(strEncrypt, valueEncrypt) << std::endl;
-
-
 
     return 0;
 }
