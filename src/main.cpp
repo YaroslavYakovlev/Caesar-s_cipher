f47db0e6c872c8fde895d49eaf73f35148fb8853
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-02-27 13:44:57 +0500

    исправлена ошибка в циклическом кодир/декодир

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,42 @@
 #include <iostream>
 
 std::string encryptCaesar(std::string str, int value){
-    char final = 'z'- value + 1;
     for(int i = 0; i < str.length(); i++){
-        if(str[i] < 'a' && str[i] > 'z'){
-            if(str[i] < final){
-                str[i] += value;
-            }else{
-                str[i] = 'a' + str[i] - final;
-            }
-        }else{
-            str = "ERROR Cipher";
-        }  
+        // if(str[i] < 'a' && str[i] > 'z'){
+            if ((str[i] += value) > 'z') str[i] -= 'z'-'a';
+        // }else{
+        //     str = "ERROR Cipher";
+        // }  
     }
     return str;
 }
 
 std::string decryptCaesar(std::string str, int value){
-    char start = 'a' + value - 1;
     for(int i = 0; i < str.length(); i++){
-        if(str[i] < 'a' && str[i] > 'z'){
-            if(str[i] > start){
-                str[i] -= value;
-            }else { 
-                str[i] = 'z' - str[i] + start;
-            }
-        }else{
-            str = "ERROR Decryption";
-        }
+        // if(str[i] < 'a' && str[i] > 'z'){
+            if ((str[i] -= value) < 'a') str[i] += 'z'-'a';
+        // }else{
+        //     str = "ERROR Decryption";
+        // }
     }
     return str;
 } 
 
 int main(){
-    std::string strEncrypt, strDecrypt;
-    int valueEncrypt, valueDecrypt;
+    std::string string;
+    int value;
 
     std::cout << "Enter the string to encode" << std::endl;
-    std::cin >> strEncrypt;
+    std::cin >> string;
     std::cout << "Enter the offset value" << std::endl;
-    std::cin >> valueEncrypt;
-    std::cout << "Cipher: " << encryptCaesar(strEncrypt, valueEncrypt) << std::endl;
+    std::cin >> value;
+    std::cout << "Cipher: " << encryptCaesar(string, value) << std::endl;
 
     std::cout << "Enter the string to decrypt" << std::endl;
-    std::cin >> strEncrypt;
+    std::cin >> string;
     std::cout << "Enter the offset value" << std::endl;
-    std::cin >> valueEncrypt;
-    std::cout << "Decryption: " << decryptCaesar(strEncrypt, valueEncrypt) << std::endl;
+    std::cin >> value;
+    std::cout << "Decryption: " << decryptCaesar(string, value) << std::endl;
 
     return 0;
 }
